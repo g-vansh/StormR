fadd8fdc94212f72c9132e1e5b2161189e51a853
Vansh Gupta <vg224@cornell.edu>
2023-05-07 11:18:40 -0400

    Added Functions

--- R/interpolate_storm_track.R
+++ R/interpolate_storm_track.R
@@ -9,21 +9,32 @@ interpolate_storm_track <- function(df){
     # Create a new dataframe to store the interpolated data
     df_interpolated <- data.frame()
 
+    # Convert latitude and longitude to numeric
+    df$Latitude <- as.numeric(df$Latitude)
+    df$Longitude <- as.numeric(df$Longitude)
+
     # Loop through each row in the dataframe
     for(i in 1:(nrow(df)-1)){
 
         # Calculate the time difference between the current row and the next row
-        time_diff <- difftime(df$time[i+1], df$time[i], units = "mins")
+        time_prev <- as.POSIXct(paste(toString(df$Date[i]), toString(df$Time[i]), sep = ""), format = "%Y%m%d%H%M")
+        time_next <- as.POSIXct(paste(toString(df$Date[i+1]), toString(df$Time[i+1]), sep = ""), format = "%Y%m%d%H%M")
+        time_diff <- as.numeric(difftime(time_next, time_prev, units = "mins"))
 
         # If the time difference is greater than 30 minutes, interpolate the data
         if(time_diff > 30){
+
+            # If i = 1, then we need to add the first row to the dataframe
+            if(i == 1){
+                df_interpolated <- rbind(df_interpolated, data.frame(df[i,]))
+            }
 
             # Calculate the number of 30 minute increments between the two rows
             num_increments <- time_diff / 30
 
             # Calculate the latitude and longitude difference between the two rows
-            lat_diff <- df$latitude[i+1] - df$latitude[i]
-            lon_diff <- df$longitude[i+1] - df$longitude[i]
+            lat_diff <- df$Latitude[i+1] - df$Latitude[i]
+            lon_diff <- df$Longitude[i+1] - df$Longitude[i]
 
             # Calculate the latitude and longitude increment between the two rows
             lat_increment <- lat_diff / num_increments
@@ -33,37 +44,55 @@ interpolate_storm_track <- function(df){
             for(j in 1:(num_increments-1)){
 
                 # Calculate the new latitude and longitude
-                new_lat <- df$latitude[i] + (lat_increment * j)
-                new_lon <- df$longitude[i] + (lon_increment * j)
+                new_lat <- df$Latitude[i] + (lat_increment * j)
+                new_lon <- df$Longitude[i] + (lon_increment * j)
 
-                # Calculate the new time
-                new_time <- df$time[i] + (30 * j)
+                # Calculate the new time and date as POSIXct
+                new_time <- as.POSIXct(paste(toString(df$Date[i]), toString(df$Time[i]), sep = ""), format = "%Y%m%d%H%M") + (30 * j * 60)
+                new_date <- as.Date(new_time)
 
-                # Create a new row with the interpolated data
-                new_row <- data.frame(STORM_ID = df$STORM_ID[i],
-                                      NAME = df$NAME[i],
-                                      ISO_TIME = new_time,
-                                      LAT = new_lat,
-                                      LON = new_lon,
-                                      WIND = df$WIND[i],
-                                      PRESSURE = df$PRESSURE[i],
-                                      STORM_TYPE = df$STORM_TYPE[i],
-                                      STORM_SPEED = df$STORM_SPEED[i],
-                                      STORM_DIR = df$STORM_DIR[i],
-                                      STORM_DIST = df$STORM_DIST[i],
-                                      STORM_BASIN = df$STORM_BASIN[i],
-                                      STORM_SUBBASIN = df$STORM_SUBBASIN[i],
-                                      STORM_SEASON = df$STORM_SEASON[i],
-                                      STORM_YEAR = df$STORM_YEAR[i],
-                                      STORM_MONTH = df$STORM_MONTH[i],
-                                      STORM_DAY = df$STORM_DAY[i],
-                                      STORM_HOUR = df$STORM_HOUR
-                                        )
+                # Convert the new time and date to the correct format
+                new_time <- format(new_time, "%H%M")
+                new_date <- format(new_date, "%Y%m%d")
+
+                # Add these columns to the new row
+                new_row <- data.frame(
+                                        StormID = df$StormID[i],
+                                        StormName = df$StormName[i],
+                                        Date = new_date,
+                                        Time = new_time,
+                                        Latitude = new_lat,
+                                        Longitude = new_lon,
+                                        RecordIdentifier = df$RecordIdentifier[i],
+                                        SystemStatus = df$SystemStatus[i],
+                                        MaximumWind = df$MaximumWind[i],
+                                        MinimumPressure = df$MinimumPressure[i],
+                                        "34NE" = df$`34NE`[i],
+                                        "34SE" = df$`34SE`[i],
+                                        "34SW" = df$`34SW`[i],
+                                        "34NW" = df$`34NW`[i],
+                                        "50NE" = df$`50NE`[i],
+                                        "50SE" = df$`50SE`[i],
+                                        "50SW" = df$`50SW`[i],
+                                        "50NW" = df$`50NW`[i],
+                                        "64NE" = df$`64NE`[i],
+                                        "64SE" = df$`64SE`[i],
+                                        "64SW" = df$`64SW`[i],
+                                        "64NW" = df$`64NW`[i],
+                                        RadiusMaxWind = df$RadiusMaxWind[i])
 
                 # Add the new row to the dataframe
                 df_interpolated <- rbind(df_interpolated, new_row)
             }
+
+            # Add the last row to the dataframe
+            df_interpolated <- rbind(df_interpolated, data.frame(df[i+1,]))
+
+        } else {
+            # If the time difference is less than 30 minutes, add the row to the dataframe
+            df_interpolated <- rbind(df_interpolated, data.frame(df[i,]))
         }
+
     }
 
     return(df_interpolated)
--- R/plot_storm_track.R
+++ R/plot_storm_track.R
@@ -7,42 +7,57 @@
 #' @return A map of storm tracks.
 #' @export
 plot_storm_track <- function(df_list){
-    # Use the maps package to get country and state boundaries
-    library(maps)
 
-    # Get country boundaries
-    world <- map_data("world")
+    # Row bind the data frames in the list
+    df <- dplyr::bind_rows(df_list)
 
-    # Get state boundaries
-    states <- map_data("state")
+    # Plot the storm tracks
+    plot <- plot_storms(df)
 
     # Plot the map
-    ggplot() +
-        # Add country boundaries
-        geom_polygon(data = world, aes(x = long, y = lat, group = group), fill = "grey", color = "black") +
-        # Add state boundariess
-        geom_polygon(data = states, aes(x = long, y = lat, group = group), fill = "grey", color = "black") +
-        # Add storm tracks
-        geom_path(data = df_list[[1]], aes(x = long, y = lat, group = storm_id), color = "red") +
-        geom_path(data = df_list[[2]], aes(x = long, y = lat, group = storm_id), color = "blue") +
-        geom_path(data = df_list[[3]], aes(x = long, y = lat, group = storm_id), color = "green") +
-        geom_path(data = df_list[[4]], aes(x = long, y = lat, group = storm_id), color = "orange") +
-        geom_path(data = df_list[[5]], aes(x = long, y = lat, group = storm_id), color = "purple") +
-        # Add a title
-        ggtitle("Storm Tracks") +
-        # Add a caption
-        labs(caption = "Source: National Hurricane Center") +
-        # Set the x-axis label
-        xlab("Longitude") +
-        # Set the y-axis label
-        ylab("Latitude") +
-        # Set the x-axis limits
-        xlim(-100, -10) +
-        # Set the y-axis limits
-        ylim(0, 60) +
-        # Set the theme
-        theme_bw()
-    
-    # Return the plot
+    return(plot)
+
+}
+
+plot_storms <- function(df){
+    # Use the maps package to get country and state boundaries
+    library(maps)
+    library(ggplot2)
+
+    # Convert Latitude and Longitude to numeric
+    df$Latitude <- as.numeric(df$Latitude)
+    df$Longitude <- as.numeric(df$Longitude)
+
+    # Get country boundaries
+    world <- ggplot2::map_data("world")
+
+    # Get state boundaries
+    states <- ggplot2::map_data("state")
+
+    # Plot the map
+    plot <- ggplot() +
+    # Add country boundaries
+    geom_polygon(data = world, aes(x = long, y = lat, group = group), fill = "grey", color = "black") +
+    # Add state boundaries
+    geom_polygon(data = states, aes(x = long, y = lat, group = group), fill = "grey", color = "black") +
+    # Add storm tracks
+    geom_path(data = df, aes(x = Longitude, y = Latitude, group = StormID, color = StormID), linewidth = 1) +
+    # Add a title
+    ggtitle("Storm Tracks") +
+    # # Add a caption
+    labs(caption = "Source: Atlantic Oceanographic and Meteorological Laboratory") +
+    # # Set the x-axis label
+    xlab("Longitude") +
+    # # Set the y-axis label
+    ylab("Latitude") +
+    # # Set the theme
+    theme_bw() +
+    # Set the x-axis limits to everything below 0
+    scale_x_continuous(limits = c(-150, -25)) +
+    # Set the y-axis limits to everything above 0
+    scale_y_continuous(limits = c(10, 80)) +
+    # Set the legend title
+    labs(color = "Storm ID")
+
     return(plot)
 }
